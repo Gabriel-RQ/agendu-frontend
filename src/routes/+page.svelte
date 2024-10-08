a4fb2b235a09b15143757d29b8d3741f4fdca613
Gabriel RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2024-10-07 21:58:18 -0300

    Exemplos dos componentes.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,18 @@
 <script>
-    import { Button, TextButton } from "$lib";
+    import { Button, TextButton, Input } from "$lib";
+    import Fa from "svelte-fa";
+    import {faHouse, faEnvelope} from "@fortawesome/free-solid-svg-icons";
 </script>
 
 <main>
     <div>
+        <Fa icon={faHouse} scale=2 size="lg" color="red" />
         <Button class="text-1_75 font-medium" expanded>Botão primário</Button>
         <Button class="text-1_75 font-medium" secondary expanded>Botão secundário</Button>
         <TextButton class="text-1_75 font-medium" href="/teste">Botão de texto</TextButton>
+        <Input expanded  value="teste">
+            <Fa slot="icon" icon={faEnvelope} scale=1.5 size="lg" />
+        </Input>
     </div>
 </main>
 
