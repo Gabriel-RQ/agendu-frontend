8ef16e1be4c7a626ef17fd3419483ad85e6f2174
Gabriel RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2024-11-04 22:33:01 -0300

    update: Corrige detalhes e adiciona mensagens de validação no formulário de login.

--- src/routes/(login)/login/+page.svelte
+++ src/routes/(login)/login/+page.svelte
@@ -3,8 +3,12 @@
     import Fa from "svelte-fa";
     import { faArrowLeft, faEnvelope, faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 	import { scale } from "svelte/transition";
+	import { enhance } from "$app/forms";
     
     let showPassword = false;
+
+    /** @type {import('./$types').ActionData} */
+    export let form;
 </script>   
 
 
@@ -12,9 +16,9 @@
 
     <span class="titulo flex-row-center">
         <h1 class="text-1_75 text-green">Informações de Login</h1>
-    </span>     
+    </span>                                  
 
-    <form class="text-black flex-column-center full-width" action="">
+    <form class="text-black flex-column-center full-width" method="post" use:enhance >
 
         <div class="input-wrapper flex-column full-width">
             <label for="email">Endereço de e-mail</label>
@@ -32,11 +36,15 @@
                 </button>
             </Input>
 
+            <!-- TODO: Mudar tag para a correta quando for implementar -->
             <p id="forgot-password">Esqueci minha senha</p>
         </div>
 
-        <Button class="font-medium text-1_15" expanded>Login</Button>
+        <Button class="font-medium text-1_15" expanded type="submit">Login</Button>
 
+        {#if form?.error}
+            <p in:scale class="text-red">{form.error}</p>
+        {/if}
     </form>
 
     <TextButton highlight id="back-button" class="flex-row-center full-width" href="/">
@@ -62,9 +70,10 @@
     }
 
     #forgot-password {
+        align-self: flex-end;
         color: var(--primary-color);
         cursor: pointer;
-        text-align: right;
+        width: fit-content;
     }
 
     :global(#back-button) {
