cdd077b57047bba7a9feae2173cfb94a7b92abaf
Gabriel RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2024-11-24 14:58:43 -0300

    refactor: Mudança na estruturação do código da página inicial

---
+++ src/routes/(authenticated)/home/+page.svelte
@@ -0,0 +1,41 @@
+<script>
+	import {
+		faCalendarAlt,
+		faClock,
+		faChalkboardTeacher,
+		faChartBar,
+		faCheck,
+		faBook
+	} from '@fortawesome/free-solid-svg-icons';
+	import Card from './Card.svelte';
+
+	const menuCards = [
+		{ icon: faCalendarAlt, title: 'Quadro de horários' },
+		{ icon: faClock, title: 'Períodos' },
+		{ icon: faChalkboardTeacher, title: 'Professores' },
+		{ icon: faChartBar, title: 'Planilhas públicas' },
+		{ icon: faCheck, title: 'Atividades' },
+		{ icon: faBook, title: 'Turmas' }
+	];
+</script>
+
+<section class="container flex-column-center full-height">
+	<!-- Área de navegação principal em cards -->
+	<div class="grid">
+		{#each menuCards as { icon, title }}
+			<Card {icon} {title} />
+		{/each}
+	</div>
+</section>
+
+<style>
+	.grid {
+		box-shadow: #000 0px 0px 10px -5px;
+		border-radius: 1rem;
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		gap: 10px;
+		margin-bottom: 20px;
+		padding: 0.75rem;
+	}
+</style>
