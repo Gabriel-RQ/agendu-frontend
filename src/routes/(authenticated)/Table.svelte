996fbc456676cadaf3802bf5b4b90717718dc423
Gabriel RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2024-12-05 21:31:11 -0300

    feat: Adiciona componentes e estilos para tabelas e páginas com tabelas

---
+++ src/routes/(authenticated)/Table.svelte
@@ -0,0 +1,45 @@
+<script>
+	/** @type { string[] } */
+	export let headers = [];
+	let className = '';
+	export { className as class };
+</script>
+
+<table class="full-width table {className}">
+	<thead>
+		<tr>
+			{#each headers as header}
+				<th>{header}</th>
+			{/each}
+		</tr>
+	</thead>
+
+	<tbody>
+		<slot />
+	</tbody>
+</table>
+
+<style>
+	table {
+		table-layout: fixed;
+		text-align: left;
+	}
+
+	thead {
+		background: #eef3f7;
+		height: 2rem;
+	}
+
+	th {
+		padding: 0.75rem 0.5rem;
+	}
+
+	:global(td) {
+		padding: 0.5rem;
+	}
+
+	:global(.table tr):hover,
+	:global(.table tr):active {
+		background-color: #f1f1f1;
+	}
+</style>
---
+++ src/routes/(authenticated)/TablePage.svelte
@@ -0,0 +1,50 @@
+<script>
+	import Table from './Table.svelte';
+
+	/** @type { string[] } */
+	export let headers;
+	/** @type {string} */
+	export let title;
+</script>
+
+<section class="full-height container text-black">
+	<div class="inner-container full-height">
+		<header>
+			<div class="wrapper">
+				<h1>{title}</h1>
+				<slot name="header-content" />
+			</div>
+			<slot name="header-searchbar" />
+		</header>
+
+		<Table {headers}>
+			<slot name="table-content" />
+		</Table>
+	</div>
+</section>
+
+<style>
+	.inner-container {
+		display: contents;
+	}
+
+	header {
+		margin-bottom: 1rem;
+	}
+
+	header .wrapper {
+		align-items: center;
+		display: flex;
+		justify-content: space-between;
+		margin-bottom: 1rem;
+	}
+
+	@media screen and (min-width: 992px) {
+		.inner-container {
+			box-shadow: #000 0px 0px 10px -5px;
+			border-radius: 1rem;
+			display: block;
+			padding: 0.75rem;
+		}
+	}
+</style>
